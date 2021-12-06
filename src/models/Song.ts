1b411d73b5f630b449eb83e99b8919daa4dfe2dd
Rigoberto Caionda <rigobertocaionda98@gmail.com>
2021-12-06 13:36:00 +0100

    Adding files

---
+++ src/models/Song.ts
@@ -0,0 +1,33 @@
+import { Model, DataTypes } from 'sequelize';
+import { sequelize } from '../instances/mysql';
+
+export interface SongInstance extends Model {
+	id: number;
+	title: string;
+	body: string;
+	singer: string;
+	image: string;
+}
+
+export const Song = sequelize.define<SongInstance>('Song', {
+	id: {
+		primaryKey: true,
+		autoIncrement: true,
+		type: DataTypes.INTEGER
+	},
+	title: {
+		type: DataTypes.STRING
+	},
+	body: {
+		type: DataTypes.STRING
+	},
+	singer: {
+		type: DataTypes.STRING
+	},
+	image: {
+		type: DataTypes.STRING
+	}
+},{
+	tableName: 'songs',
+	timestamps: false
+});
